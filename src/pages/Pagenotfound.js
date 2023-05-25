31e269fb02f61913bd1276ec828b4fa30bdb3512
Pankaj Pal <munapal75917@gmail.com>
2023-05-25 11:44:34 +0530

    Resturent

---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>Page not Found Page</h1>
+    </Layout>
+  )
+}
+
+export default Pagenotfound
